38ccd4b598d05458af01b8e5adf0d36b9b6a86e7
Sudip Limbu <4slimbu@gmail.com>
2022-07-16 12:58:13 +0545

    initial commit

---
+++ tests/test-app.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Testplugin
+ */
+
+/**
+ */
+class MyTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_reverse() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
---
+++ tests/unit/test-app.php
@@ -0,0 +1,21 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SampleTest
+ *
+ * @package Testplugin
+ */
+
+/**
+ */
+class AppTest extends TestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_reverse() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
